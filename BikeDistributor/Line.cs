dcd89860450132b53b4e86189af4888eb5a6d9bb
brainbolt <brainbolts@gmail.com>
2018-01-11 12:35:45 -0800

    replace temp var with query, move line price logic

--- BikeDistributor/Line.cs
+++ BikeDistributor/Line.cs
@@ -10,5 +10,34 @@
 
         public Bike Bike { get; private set; }
         public int Quantity { get; private set; }
+
+        public double GetAmount()
+        {
+            double thisAmount = 0d;
+
+            switch (Bike.Price)
+            {
+                case Bike.OneThousand:
+                    if (Quantity >= 20)
+                        thisAmount += Quantity * Bike.Price * .9d;
+                    else
+                        thisAmount += Quantity * Bike.Price;
+                    break;
+                case Bike.TwoThousand:
+                    if (Quantity >= 10)
+                        thisAmount += Quantity * Bike.Price * .8d;
+                    else
+                        thisAmount += Quantity * Bike.Price;
+                    break;
+                case Bike.FiveThousand:
+                    if (Quantity >= 5)
+                        thisAmount += Quantity * Bike.Price * .8d;
+                    else
+                        thisAmount += Quantity * Bike.Price;
+                    break;
+            }
+
+            return thisAmount;
+        }
     }
 }
