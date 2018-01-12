bca61504690b7898e9b2e8b59c8c5fdf61818d84
brainbolt <brainbolts@gmail.com>
2018-01-11 16:24:18 -0800

    add factory for receipt creators
    - add ReceiptCreatorFactory test coverage
    
    This is a simple factory method implementation for creating
    ReceiptCreators. In a production application, a full Factory Pattern or
    Abstract Factory Pattern implementation may be appropriate.

---
+++ BikeDistributor.Test/ReceiptCreatorFactoryTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BikeDistributor.Test
+{
+    [TestClass]
+    public class ReceiptCreatorFactoryTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReceiptCreatorFactory_GetReceiptCreator_ShouldThrow_ArgumentException_WhenPassEmptyString()
+        {
+            var factory = new ReceiptCreatorFactory().MakeReceiptCreator("");
+        }
+
+        [TestMethod]
+        public void ReceiptCreatorFactory_GetReceiptCreator_ShouldReturn_TextReceiptCreatorType_WhenPassed_default()
+        {
+            var creator = new ReceiptCreatorFactory().MakeReceiptCreator("default");
+            Assert.IsInstanceOfType(creator, typeof(TextReceiptCreator));
+        }
+
+        [TestMethod]
+        public void ReceiptCreatorFactory_GetReceiptCreator_ShouldReturn_TextReceiptCreatorType_WhenPassed_text()
+        {
+            var creator = new ReceiptCreatorFactory().MakeReceiptCreator("text");
+            Assert.IsInstanceOfType(creator, typeof(TextReceiptCreator));
+        }
+
+        [TestMethod]
+        public void ReceiptCreatorFactory_GetReceiptCreator_ShouldReturn_HtmlReceiptCreatorType_WhenPassed_html()
+        {
+            var creator = new ReceiptCreatorFactory().MakeReceiptCreator("html");
+            Assert.IsInstanceOfType(creator, typeof(HtmlReceiptCreator));
+        }
+    }
+}
---
+++ BikeDistributor/ReceiptCreatorFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BikeDistributor
+{
+    public class ReceiptCreatorFactory
+    {
+        public IReceiptCreator MakeReceiptCreator(string receiptType)
+        {
+            IReceiptCreator creator;
+
+            switch (receiptType)
+            {
+                case "default":
+                    creator = new TextReceiptCreator();
+                    break;
+                case "text":
+                    creator = new TextReceiptCreator();
+                    break;
+                case "html":
+                    creator = new HtmlReceiptCreator();
+                    break;
+                default:
+                    throw new ArgumentException($"invalid receipt type: {receiptType}");
+            }
+
+            return creator;
+        }
+    }
+}
