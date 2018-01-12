8b8fdfdf07568b0d82e5e75141d163fd365be5c0
brainbolt <brainbolts@gmail.com>
2018-01-11 16:46:41 -0800

    add Line test coverage
    on the face, it appears that this is testing the implementation instead
    of the result, but since all the GetAmount() method does at this point
    is delegate the calculation of the amount to the IBike instance, it is
    important to ensure that line.GetAmount() does its part properly.

---
+++ BikeDistributor.Test/LineTest.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BikeDistributor.Test
+{
+    [TestClass]
+    public class LineTest
+    {
+        [TestMethod]
+        public void Line_GetAmount_ShouldCall_Bike_GetAmount()
+        {
+            var bike = new MockBikeA();
+            var line = new Line(bike, 0);
+
+            Assert.IsFalse(bike.GetHasBeenCalled());
+
+            line.GetAmount();
+
+            Assert.IsTrue(bike.GetHasBeenCalled());
+        }
+
+        [TestMethod]
+        public void Line_GetAmount_ShouldPassQuantityTo_Bike_GetAmount()
+        {
+            int quantity = 34;
+            var bike = new MockBikeB();
+            var line = new Line(bike, quantity);
+            line.GetAmount();
+
+            Assert.AreEqual(quantity, bike.GetQuantity());
+        }
+
+        #region Mock Objects
+
+        class MockBikeA : IBike
+        {
+            bool hasBeenCalled;
+            public string Brand => throw new NotImplementedException();
+
+            public string Model => throw new NotImplementedException();
+
+            public int Price => throw new NotImplementedException();
+
+            public double GetAmount(int quantity)
+            {
+                hasBeenCalled = true;
+                return 0d;
+            }
+
+            public bool GetHasBeenCalled()
+            {
+                return hasBeenCalled;
+            }
+        }
+
+        class MockBikeB : IBike
+        {
+            int _quantity;
+            public string Brand => throw new NotImplementedException();
+
+            public string Model => throw new NotImplementedException();
+
+            public int Price => throw new NotImplementedException();
+
+            public double GetAmount(int quantity)
+            {
+                _quantity = quantity;
+                return 0d;
+            }
+
+            public int GetQuantity()
+            {
+                return _quantity;
+            }
+        }
+
+        #endregion
+    }
+}
